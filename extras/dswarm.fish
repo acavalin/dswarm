e7cd40103d3c8cdd054157324dfa4367f74097ba
Alberto Cavalin <acavalin@users.noreply.github.com>
2024-09-16 18:21:57 +0200

    * add docker-machine commands * add fish shell wrapper for docker-machine commands

---
+++ extras/dswarm.fish
@@ -0,0 +1,20 @@
+# place in ~/.config/fish/functions/
+# from https://github.com/oh-my-fish/plugin-docker-machine/
+function dswarm -a cmd -d 'Select a Docker context'
+  switch "$cmd"
+    case 'use'
+      set -e argv[1]
+      command dswarm context env $argv fish | source
+    case 'unset'
+      command dswarm context unset fish | source
+    case '*'
+      command dswarm $argv
+      
+      if not count $argv > /dev/null
+        echo ""
+        echo -e "### switch context (fish shell)"
+        echo -e " use              <context-name>"
+        echo -e " unset"
+      end
+  end
+end
